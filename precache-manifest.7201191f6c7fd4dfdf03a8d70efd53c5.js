9bd3c86a3c8802a829ad9f0a483f55fcdd72f2ae
kpls72s <siavash.araghi@yahoo.com>
2022-01-13 23:00:13 +0330

    Updates

---
+++ precache-manifest.7201191f6c7fd4dfdf03a8d70efd53c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5f36b5c9a8e6938f53148fd7d6c7e0e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3027ba51ce66c5a12c8",
+    "url": "/static/css/main.fc3f2cfe.chunk.css"
+  },
+  {
+    "revision": "f4311ce96294c924ded9",
+    "url": "/static/js/2.c74e4280.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c74e4280.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3027ba51ce66c5a12c8",
+    "url": "/static/js/main.884c43d8.chunk.js"
+  },
+  {
+    "revision": "bb5b94e5bf8a2ac4b1d8",
+    "url": "/static/js/runtime-main.6fd1fea2.js"
+  }
+]);
